c5902f821931d5c2f4046ac5e7be5eff01a9dcd1
Vimal <arvimal@yahoo.in>
2017-12-26 06:50:55 +0530

    * Day 7 of #100DaysofCode
    	* Escaping special characters
    
    * Updated README.md

---
+++ Day-007/01-special-characters.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+# Strings can contain special characters.
+# New lines are denoted with `n`
+# Tabs are denoted with `t`.
+
+# New line
+print("I have a \nnew line in this sentence")
+
+# Tab
+print("This is a \t tabbed sentence!")
+
+# Backslashes
+print("There is a \ in this sentence!")
+
+# Backslashes can error out if not escaped properly.
+print("This is a \")
+# Such instances have to be escaped specifically.
+print("This is a \\")
--- README.md
+++ README.md
@@ -41,3 +41,7 @@ To guide me through the very basics and to the STL, I will be using the awesome 
 ### Day 6
 1. Conditionals (if/elif/else)
 2. Boolean operations (OR / AND / NOT)
+
+### Day 7
+1. Escaping special characters
+2.
