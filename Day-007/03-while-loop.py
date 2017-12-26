b8c5912f47a3b5086f373b688c768d907f689c55
Vimal <arvimal@yahoo.in>
2017-12-26 21:29:55 +0530

    * Updated Day-007/03-while-loop.py

--- Day-007/03-while-loop.py
+++ Day-007/03-while-loop.py
@@ -7,3 +7,19 @@ i = 0
 while i < 10:
     print(i)
     i += 2
+
+# Checking for a `True` condition using `while`
+# will always be running.
+
+#while True:
+#   print(">>>")
+
+# break
+# `break` is used to break out of a loop, usually a while loop,
+# when another condition is met.
+
+while i < 10:
+    print(i)
+    if i == 5:
+        break
+    i += 1
