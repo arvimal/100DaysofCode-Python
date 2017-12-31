89c885d24d6251e7037bd196296974d1d42c9dbb
Vimal <arvimal@yahoo.in>
2017-12-31 22:11:59 +0530

    * Day 12 of #100DaysofCode
    	* Deleted Day-012/03-csv-module-2.py, lacking completion.

--- Day-012/03-csv-module-2.py
+++
@@ -1,8 +0,0 @@
-/bin/bash /bin/echo
-    "first_name,last_name,address,city,state,zip_code\n
-    Tyrese,Hirthe,1404 Turner Ville,Strackeport,NY,19106-8813\n
-    Jules,Dicki,2410 Estella Cape Suite 061,Lake Nickolasville,ME,00621-7435\n
-    Dedric,Medhurst,6912 Dayna Shoal,Stiedemannberg,SC,43259-2273" >> /tmp/new.txt
-
-
-
