2234f81ea7dca869232abfbff841da3981ac40ce
Vimal <arvimal@yahoo.in>
2017-12-31 20:36:44 +0530

    * Day 12 of #100DaysofCode
    	* Added Day-012/01-introspection.py

---
+++ Day-012/01-introspection.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+"""
+Introspection is the process of understanding oneself.
+Here, we use Python's builtins to understand about Python.
+This can be helpful in case the module comes with not-so-good
+documentation.
+
+The most useful builtins to understand about an object
+
+type
+dir
+help
+
+* type
+`type` helps to understand what sort of class has the instance
+been instantiated from.
+
+* dir
+`dir()` helps to list the methods available for that instance.
+This actually lists the methods from the class which the instance
+was instantiated from.
+It is also useful to list the names in the current namespace, if
+no args are passed to dir().
+
+* help()
+`help(<module-name>)` prints the document strings of a builtin or module.
+Without arguments, it presents a `help>` prompt which can be used to
+navigate the help system in built.
+"""
+
+
+a = "test"
+print(type(a))
+
+import sys
+dir(sys)
+
+help(sys)
