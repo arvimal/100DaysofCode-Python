b20bc8d858af9a1f6158affdb663f758bcc2f1c9
Vimal <arvimal@yahoo.in>
2018-01-04 22:46:26 +0530

    * Day 16 of #100DaysofCode
    	* Updated Day-016/03-conf_file_structure.py

--- Day-016/03-conf_file_structure.py
+++ Day-016/03-conf_file_structure.py
@@ -9,11 +9,17 @@ conf_parser.read(["example_config_1.conf", "example_config_2.conf"])
 
 for section in conf_parser.sections():
     print("Section: {}".format(section))
-    print("\tOptions: {}".format(conf_parser.options(section)))
 
-
+# The `options()` method returns a list of options under each section.
 # This prints
 # Section: main
 #    Options: ['cachedir', 'keepcache', 'debuglevel', 'logfile', 'exactarch', 'obsoletes', 'gpgcheck', 'plugins', 'installonly_limit']
 # Section: statd
 #    Options: ['debug', 'port', 'outgoing-port', 'state-directory-path']
+    print("\tOptions: {}".format(conf_parser.options(section)))
+
+# The `items()` method for conf_parser returns a dictionary,
+# with the items under each section and its values.
+    print("Options and corresponding Values in the configuration file")
+    for option, values in conf_parser.items(section):
+        print(" {}: {}".format(option, values))
