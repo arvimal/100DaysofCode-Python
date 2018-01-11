96e35db6cc67ad926ec876dacac5a5ef902bd536
Vimal A.R <arvimal@yahoo.in>
2018-01-11 21:20:01 +0530

    * Added Day-021/01-os.py

---
+++ Day-021/01-os.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+import os
+
+print("Some use cases of the `os` module.\n")
+
+print("Operating System type: {}".format(os.name))
+print("Environment variables: {}".format(os.envrion))
+print("\nos.envrion returns a dict")
