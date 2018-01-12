ae8aacac44ec86a21163980836a3798076fb15c2
Vimal A.R <arvimal@yahoo.in>
2018-01-12 20:18:44 +0530

    * Day 23 of #100DaysofCode
    	* Added Day-023/01-os.py

---
+++ Day-023/01-os.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import os
+
+print("Checking if /tmp/folder_1 exists, deleting!")
+try:
+    os.rmdir("/tmp/folder_1")
+except OSError:
+    pass
+print("Changing to /tmp/")
+os.chdir("/tmp")
+print("Creating folder_1")
+os.mkdir("folder_1")
+print("Deleting /tmp/folder_1")
+os.rmdir("/tmp/folder_1")
+
+print("\nRename a folder using os.rename()")
+print("Creating /tmp/dir_1")
+os.mkdir("/tmp/dir_1")
+print("Checking if the rename worked or not")
+try:
+    os.rename("/tmp/dir_1", "/tmp/dir_2")
+except:
+    print("Rename failed.")
+print("os.renames() helps to rename dirs and files recursively")
+print("/tmp/dir_1 exists: {}".format(os.path.exists("/tmp/dir_1")))
+print("/tmp/dir_2 exists: {}".format(os.path.exists("/tmp/dir_2")))
