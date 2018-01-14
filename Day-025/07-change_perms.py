89d9efc1cea37c986198f51251ffa0c665cf6b7b
Vimal A.R <arvimal@yahoo.in>
2018-01-14 16:03:19 +0530

    * Day 25 of #100DaysofCode
    	* Modified Day-025/07-change_perms.py

--- Day-025/07-change_perms.py
+++ Day-025/07-change_perms.py
@@ -7,6 +7,20 @@ FOLDER = "/tmp/"
 FILE = "testing.txt"
 FILE_PATH = FOLDER + FILE
 
+# Removing the file at the start, to
+# avoid errors while creating it.
+if os.path.exists(FILE_PATH):
+    os.unlink(FILE_PATH)
+
 with open(FILE_PATH, "w") as fd:
     fd.write("Hello World!")
 
+current_perms = stat.S_IMODE(os.stat(FILE_PATH).st_mode)
+print("Current permissions: {}".format(current_perms))
+if not os.access(FILE_PATH, os.X_OK):
+    print("No execute permissions for {}".format(FILE_PATH))
+    print("Adding execute permissions.")
+    new_perms = current_perms | stat.S_IXUSR
+
+os.chmod(FILE_PATH, new_perms)
+print("New permissions: {}".format(new_perms))
