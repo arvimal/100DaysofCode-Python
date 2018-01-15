3f6045aa7806d01da8cbc1322a7ff47eea142f85
Vimal A.R <arvimal@yahoo.in>
2018-01-15 10:14:31 +0530

    * Day 26 of #100DaysofCode
    	* Added Day-026/01-smtplib.py

---
+++ Day-026/01-smtplib.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+# smptlib is the module through which
+# we can interact with an SMTP server.
+
+# It can be used to authenticate with mail servers,
+# send mails, retrieve mails etc..
+
+import smtplib
+
+HOST = "smtp.mail.yahoo.com"
+SUBJECT = "Testing smtplib."
+TO = "arvimal@yahoo.in"
+FROM = "arvimal@yahoo.in"
+PORT = 465
+CONTENT = "Hello, how are you today?"
+
+BODY = "\r\n".join((
+    "From: {}".format(FROM),
+    "To: {}".format(TO),
+    "Subject: {}".format(SUBJECT),
+    "",
+    CONTENT
+))
+
+server = smtplib.SMTP(HOST, PORT)
+server.sendmail(FROM, [TO], BODY)
+server.quit()
