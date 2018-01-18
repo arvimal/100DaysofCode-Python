f7a3095e35c81f66d66ec6ec6cb4199b09ee1c11
Vimal A.R <arvimal@yahoo.in>
2018-01-18 22:34:59 +0530

    * Day 29 of #100DaysofCode
    	* Updated Day-029/01-subprocess-call.py

--- Day-029/01-subprocess-call.py
+++ Day-029/01-subprocess-call.py
@@ -6,5 +6,14 @@
 import subprocess
 
 # The `call()` function/method
-
+# This enables to call binaries/scripts etc.
+# This returns a return code, which can be captured
+# in a variable
 subprocess.call("/usr/bin/terminator")
+# The return status is populated once the process terminates
+status = subprocess.call("/usr/bin/gedit")
+if status == 0:
+    print("Process start a success!")
+else:
+    print("Process start a failure")
+print("Return status: {}".format(status))
