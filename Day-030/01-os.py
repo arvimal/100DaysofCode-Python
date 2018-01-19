b12ecb42af826ddead9ea956c941f31c5fe1cdd0
Vimal A.R <arvimal@yahoo.in>
2018-01-19 23:49:07 +0530

    * Day 30 of #100DaysofCode
    	* Added Day-030/01-os.py

---
+++ Day-030/01-os.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import subprocess
+
+# Use the `run()` method to execute a command without interaction.
+command = subprocess.run(['ls', '-l', '..'])
+
+# The `returncode` attribute contains the return status
+# of the command. This can either contain an error or a success state.
+print("\nReturn status: {}".format(command.returncode))
