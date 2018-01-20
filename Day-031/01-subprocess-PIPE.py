3ddd13a7add6bc01b6923104e3d61caa41583362
Vimal A.R <arvimal@yahoo.in>
2018-01-20 20:45:55 +0530

    * Day 31 of #100DaysofCode
    	* Added Day-031/01-subprocess-PIPE.py

---
+++ Day-031/01-subprocess-PIPE.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import subprocess
+
+# The calling program cannot capture the stdout and stdin
+# of the processes run by subprocess.run()
+# ie.. if we execute this file from bash, it cannot capture
+# the stdout/stdin, and thus it's lost
+# In order capture those, we can use `subprocess.PIPE` and set 
+# the `stdout` argument to capture it. 
+#
+completed = subprocess.run(["ls", "-l"], stdout=subprocess.PIPE)
+
+print("Return Code: {}".format(completed.returncode))
+
+print("\nstdout : {}".format(completed.stdout.decode('utf-8')))
