ea2abd0e51e18fc578892f9c927134bf870f97f7
Vimal A.R <arvimal@yahoo.in>
2018-01-21 22:34:12 +0530

    * Day 32 of #100DaysofCode
    	* Added Day-032/01-subprocess-Popen-read.py

---
+++ Day-032/01-subprocess-Popen-read.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import subprocess
+
+print("Read: ")
+
+process = subprocess.Popen(["echo", "to stdout"], stdout=subprocess.PIPE,)
+
+stdout_value = process.communicate()[0].decode('utf-8')
+print('stdout:', repr(stdout_value))
