09d0d7eb164bcd0102e2c89f38f4a8ba4ead982d
Vimal A.R <arvimal@yahoo.in>
2018-01-29 09:00:42 +0530

    * Day 36 of #100DaysofCode
    	* Moved Day-036/01-sys-memory_limits.py to Day-036/01-sys-reference_count.py
    	* Added Day-036/02-sys-object_size.py

---
+++ Day-036/02-sys-object_size.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+# The `sys.getsizeof()` method allows to fetch the
+# size of the objects in the namespace.
+
+import sys
+
+print("Getting an object size listing:\n")
+for i in dir():
+    print("{:>20} : {}".format(i, sys.getsizeof(i)))
+
+
+print("\nCreating new objects.")
+
+# Class
+
+
+class MyClass:
+    pass
+# Function
+
+
+def my_func():
+    pass
+
+
+# Lists:
+list_1 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+print("Getting a new object size listing:\n")
+for i in dir():
+    print("{:>20} : {}".format(i, sys.getsizeof(i)))
