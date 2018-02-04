8eb6e7e64150cf384b529daf89d2ff3d8905cff7
Vimal A.R <arvimal@yahoo.in>
2018-02-04 23:25:05 +0530

    * Day 39 of #100DaysofCode
    	* Added Day-039/01-threading.py

---
+++ Day-039/01-threading.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import random
+import time
+from threading import Thread
+
+class MyThread(Thread):
+    """
+    Learing how to use threads
+    """
+
+    def __init__(self, name):
+        Thread.__init__(self)
+        self.name = name
+
+    def run(self):
+        """
+        Run a thread that just sleeps
+        """
+        count = random.randint(1,10)
+        time.sleep(count)
+        print("{} running".format(self.name))
+
+def create_threads():
+    for num in range(10):
+        name = "Thread {}".format(num + 1)
+        my_thread = MyThread(name)
+        my_thread.start()
+
+if __name__ == "__main__":
+    create_threads()
+
