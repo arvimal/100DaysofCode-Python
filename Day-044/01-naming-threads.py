51434d291a7a4f3e15630ed836a1ff58d5d971c4
Vimal A.R <arvimal@yahoo.in>
2018-02-09 23:53:05 +0530

    * Day 44 of #100DaysofCode
    	* Added Day-044/01-naming-threads.py

---
+++ Day-044/01-naming-threads.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+# Each thread has a default name assigned to it, 
+# and this can be fetched by `threading.Thread().name`,
+# as seen in the previous day's example.
+
+# The default name can be changed by using the `name` argument 
+# to `threading.Thread(name=<custom_name>`, target=<function>)`
+
+import threading
+import time
+import pprint
+
+def my_worker():
+    print("Starting {}".format(threading.current_thread().getName()))
+    time.sleep(5)
+    print("Exiting thread {}".format(threading.current_thread().getName()))
+
+thread_list = []
+
+for i in range(10):
+    t = threading.Thread(name="Worker thread", target=my_worker)
+    thread_list.append(t)
+    t.start()
+
+pprint.pprint(thread_list)
