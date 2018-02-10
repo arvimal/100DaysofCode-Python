cbaa13eeca0bba64b15d49a4e23d946960fd91bd
Vimal A.R <arvimal@yahoo.in>
2018-02-10 23:18:03 +0530

    * Day 45 of #100DaysofCode
    	* Modified Day-045/01-threading_n_logging.py

--- Day-045/01-threading_n_logging.py
+++ Day-045/01-threading_n_logging.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python3
 
+# The `logging` module supports and can log messages
+# from multiple threads within the same process, separately.
+# Hence, it's easy to include logs from specific threads.
+
+# In short, the `logging` module is thread-safe, hence messages from
+# different threads are kept different in the logs.
+
 import threading
 import logging
 import time
 
-
+# Our function which will be called by our threads
 def my_worker_func():
     logging.debug("Starting the worker function!")
     time.sleep(5)
@@ -13,9 +20,9 @@ def my_worker_func():
 logging.basicConfig(level=logging.DEBUG,
                     format="[%(levelname)s] (%(threadName)-10s) %(message)s",)
 
-process_1 = threading.Thread(name="Worker function 1", target=my_worker_func)
-process_2 = threading.Thread(name="Worker function 2", target=my_worker_func)
+thread_1 = threading.Thread(name="Worker function 1", target=my_worker_func)
+thread_2 = threading.Thread(name="Worker function 2", target=my_worker_func)
 
-process_1.start()
-process_2.start()
+thread_1.start()
+thread_2.start()
 
