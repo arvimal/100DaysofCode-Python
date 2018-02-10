42e4974c722060d66a63f695e47527c6f1d31f2e
Vimal A.R <arvimal@yahoo.in>
2018-02-11 00:04:00 +0530

    * Day 45 of #100DaysofCode
    	* Modified Day-045/01-threading_n_logging.py
    
    	* Added Day-045/02-threading_daemons.py

--- Day-045/01-threading_n_logging.py
+++ Day-045/01-threading_n_logging.py
@@ -12,16 +12,21 @@ import logging
 import time
 
 # Our function which will be called by our threads
-def my_worker_func():
-    logging.debug("Starting the worker function!")
+def my_worker_func_1():
+    logging.debug("Starting the first worker function!")
     time.sleep(5)
-    logging.debug("Stopping the worker function!")
+    logging.debug("Stopping the first worker function!")
+
+def my_worker_func_2():
+    logging.debug("Starting the second worker function!")
+    time.sleep(5)
+    logging.debug("Stopping the second worker function!")
 
 logging.basicConfig(level=logging.DEBUG,
                     format="[%(levelname)s] (%(threadName)-10s) %(message)s",)
 
-thread_1 = threading.Thread(name="Worker function 1", target=my_worker_func)
-thread_2 = threading.Thread(name="Worker function 2", target=my_worker_func)
+thread_1 = threading.Thread(name="Worker function 1", target=my_worker_func_1)
+thread_2 = threading.Thread(name="Worker function 2", target=my_worker_func_2)
 
 thread_1.start()
 thread_2.start()
