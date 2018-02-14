c8f79daae15bb25d9ec3895cb59b3c8e1ceff810
Vimal A.R <arvimal@yahoo.in>
2018-02-14 23:44:24 +0530

    * Day 48 of #100DaysofCode
    	* Added Day-048/01-datetime.py

---
+++ Day-048/01-datetime.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+import datetime
+
+today = datetime.date.today()
+
+print("Day  : {}".format(today.day))
+print("Month: {}".format(today.month))
+print("Year : {}".format(today.year))
