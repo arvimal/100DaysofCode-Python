a4a34b9c851cf4d9a6474f5cde7239ea2d8491b2
Vimal A.R <arvimal@yahoo.in>
2018-02-16 09:00:00 +0530

    * Day 49 of #100DaysofCode
    	* Updated Day-049/01-datetime_time.py

--- Day-049/01-datetime_time.py
+++ Day-049/01-datetime_time.py
@@ -5,9 +5,17 @@
 
 import datetime
 
+# Dates are manipulated using the `date` class in `datetime` module.
+# whereas time is manipulated using the `time` class in the module.
+
 today = datetime.date.today()
 
-print("Day      : {}".format(today.day))
-print("Month    : {}".format(today.month))
-print("Year     : {}".format(today.year))
-print("today    : {}".format(today.today()))
+print("Day          : {}".format(today.day))
+print("Month        : {}".format(today.month))
+print("Year         : {}".format(today.year))
+print("Today        : {}".format(today.today()))
+
+print("Min time     : {}".format(datetime.date.min))
+print("Max time     : {}".format(datetime.date.max))
+print("Resolution   : {}".format(datetime.date.resolution))
+
