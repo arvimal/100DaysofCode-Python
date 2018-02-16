fa1aac3d0a15273cec156ae9d09b6babe9c787e7
Vimal A.R <arvimal@yahoo.in>
2018-02-16 09:00:49 +0530

    * Day 50 of #100DaysofCode
    	* Added Day-050/01-datetime_dates.py

---
+++ Day-050/01-datetime_dates.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+# Calendar dates can be worked with the `date` class in the
+# `datetime` module. [datetime.date.<methods>]
+
+# Instances of the `date` class from `datetime`, inherits methods
+# such as `day`, `month`, year` etc. 
+
+# The `today` class method of `date` in `datetime` module, can return
+# the current date.
+
+import datetime
+import pprint
+
+today = datetime.date.today()
+print("Date     : {}\n".format(today))
+
+time_tuple = today.timetuple()
+
+print("Year     : {}".format(time_tuple.tm_year))
+print("Month    : {}".format(time_tuple.tm_mon))
+print("Date     : {}".format(time_tuple.tm_mday))
+print("Weekday  : {}".format(time_tuple.tm_wday))
+print("Yearday  : {}".format(time_tuple.tm_yday))
+
