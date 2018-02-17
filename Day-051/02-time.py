533ee463b9a1401c340a52d999a0ad88865c99cd
Vimal A.R <arvimal@yahoo.in>
2018-02-17 23:58:57 +0530

    * Day 51 of #100DaysofCode
    	* Updated Day-051/02-time.py

--- Day-051/02-time.py
+++ Day-051/02-time.py
@@ -3,6 +3,8 @@
 import time
 
 print("Date/Time now {}".format(time.ctime()))
+print("Seconds from EPOCH : {}".format(time.time()))
 print("Sleeping 5 seconds")
 time.sleep(5)
 print("Date/Time now {}".format(time.ctime()))
+print("Seconds from EPOCH : {}".format(time.time()))
