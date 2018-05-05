b91b9b406788c842e833ab532e94fdf6c1571729
Vimal A.R <arvimal@yahoo.in>
2018-05-05 20:58:17 +0530

    * Added Day-068/text_wrap_poem.py

---
+++ Day-068/text_wrap_poem.py
@@ -0,0 +1,13 @@
+
+rosetti_unformatted = """
+                      Remember me when I am gone away,
+                      Gone far away into the silent land;
+                      When you can no more hold me by the hand,
+
+                      Nor I half turn to go yet turning stay.
+
+                      Remember me when no more day by day
+                      You tell me of our future that you planned:
+                      Only remember me; you understand
+                      """
+
