0ef10bb44e7284cf885c95c23e6decf097882ba5
Vimal A.R <vikumar@redhat.com>
2018-05-23 00:10:07 +0530

    Added stopwatch.py

---
+++ Day-072/stopwatch.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+# A program intented to work as a stopwatch
+
+import time
+
+input("Press `Enter` to begin. Press `Enter` to click the stopwatch. Use `Ctrl
+        + C` to quit")
+
+start_time = time.time()
+last_time = start_time
+
+print("Stopwatch started.")
+lap_count = 1
+
+
+
+
