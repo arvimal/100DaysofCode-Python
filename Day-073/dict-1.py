c47394dd16be89d39cf7ddb5f8eee67e11001842
Vimal A.R <arvimal@yahoo.in>
2018-06-06 07:23:34 +0530

    * Day 73 of #100DaysofCode
    	* Added Day-073/dict-1.py
    
    	* `break` does not work with `if` loops.
    	* It's normally used with `for` or `while` loops.
    	* `{dict}.keys()` and `{dict} iterate through the keys.

---
+++ Day-073/dict-1.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+birthdays = {
+    "Alice": "April 1",
+    "Bob": "December 12",
+    "Carol": "March 4"
+}
+
+while True:
+    name = input("\nEnter a name [blank to quit]: ")
+    if name == "":
+        break
+
+    if name in birthdays.keys():
+        # birthdays or birthdays.keys() iterate through keys
+        print("{}'s birthday is on {}\n".format(name, birthdays[name]))
+    else:
+        print("Name does not exist in database!")
+        birth_date = input("When is your friend's birthday? ")
+        birthdays[name] = birth_date
+        print("Birthday date for {} updated!".format(name))
