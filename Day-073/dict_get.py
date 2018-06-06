2627fb29cf622ca3454533d91028747a23b5b923
Vimal A.R <arvimal@yahoo.in>
2018-06-06 07:52:23 +0530

    * Day 73 of #100DaysofCode
    	* Added Day-073/dict_get.py
    
    	The `get()` method for dict() helps to return a pre-defined
    	value, if the key being called does not exist in the dict.

---
+++ Day-073/dict_get.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+"""
+The get() method for a dict is used to:
+    * Check if a key exist in dict.
+    * If the key does not exist, return a predefined value.
+
+"""
+
+birthdays = {
+    "Alice": "April 1",
+    "Bob": "December 12",
+    "Carol": "March 4"
+}
+
+# We try to list a key which is present, and not.
+
+# 1. Key does exist in the birthdays dict
+# If the key does exist, it returns the actual value for the key.
+print("{}`s birthday is on {}".format(
+    "Alice", birthdays.get("Alice", "Oct 10")))
+
+
+# 2. Key does not exist in the birthdays dict
+# If the key does not exist, it returns the predefined value set in the get()
+# method.
+print("{}`s birthday is on {}".format(
+    "Vimal", birthdays.get("Vimal", "Oct 15")))
