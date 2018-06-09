41a06633ddd30803570d06b28f41c410cc23f3d2
Vimal A.R <arvimal@yahoo.in>
2018-06-09 23:42:40 +0530

    * Day 76 of #100DaysofCode
    	* Updated Day-074/tic-tac-toe.py to a better version.

--- Day-074/tic-tac-toe.py
+++ Day-074/tic-tac-toe.py
@@ -2,9 +2,17 @@
 
 # Generate a tic-tac-toe board using dicts.
 
+"""
 theBoard = {"top-L": " ", "top-M": " ", "top-R": " ",
             "mid-L": " ", "mid-M": " ", "mid-R": " ",
             "low-L": " ", "low-M": " ", "low-R": " "}
+"""
+
+theBoard = [
+    {"top-L": " ", "top-M": " ", "top-R": " "},
+    {"mid-L": " ", "mid-M": " ", "mid-R": " ", },
+    {"low-L": " ", "low-M": " ", "low-R": " "}
+]
 
 
 def printBoard(board):
@@ -25,5 +33,46 @@ def printBoard(board):
     print("\n")
 
 
-# Get the current status of our tic-tac-toe board
-printBoard(theBoard)
+def check_win(theBoard):
+    """
+    Check for a win
+    """
+    vals = ["X", "O"]
+    #import pdb
+    # pdb.set_trace()
+    for i in vals:
+
+        if (theBoard["top-L"] and theBoard["top-M"] and theBoard["top-R"]) is i:
+            print("Player {} is the new Winner".format(i))
+        elif (theBoard["mid-L"] and theBoard["mid-M"] and theBoard["mid-R"]) is i:
+            print("Player {} is the new Winner".format(i))
+        elif (theBoard["low-L"] and theBoard["low-M"] and theBoard["low-R"]) is i:
+            print("Player {} is the new Winner".format(i))
+        else:
+            pass
+
+
+def play_loop():
+    player = 'X'
+    for i in range(9):
+        printBoard(theBoard)
+        move_location = input(
+            "Player ** {} **, enter location: ".format(player))
+        if move_location not in theBoard.keys():
+            print("  \nLocation {} not valid. Try again!".format(move_location))
+            play_loop()
+        # Set the move location to the user's input.
+        theBoard[move_location] = player
+        # Check if we've met a win condition
+        check_win(theBoard)
+        if player == "X":
+            player = "O"
+        else:
+            player = "X"
+    printBoard(theBoard)
+
+
+if __name__ == "__main__":
+    # There are two players in tic-tac-toe, "X" and "O".
+    # Both the player and their move is denoted by "X" and "O".
+    play_loop()
