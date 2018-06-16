22c7bacdbfa9bedf5665ba452d15051040e839ea
Vimal A.R <arvimal@yahoo.in>
2018-06-16 23:39:32 +0530

    * Day 79 of #100DaysofCode
    	* Started HackerRank Python series, solved first challenge.

---
+++ Day-079/hacker_rank-1.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+if __name__ == '__main__':
+    N = int(input())
+    if N % 2 != 0:
+        print("Weird")
+    elif N % 2 == 0 and N in range(2, 6):
+        print("Not Weird")
+    elif N % 2 == 0 and N in range(6, 21):
+        print("Weird")
+    elif N % 2 == 0 and N > 20:
+        print("Not Weird")
