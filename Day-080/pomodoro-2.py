a89636130b5d85ceda5e22ec979a6a8a3f6cf2e5
Vimal A.R <arvimal@yahoo.in>
2018-06-17 18:04:29 +0530

    * Day 80 of #100DaysofCode
    	* Updated Day-080/pomodoro-2.py to handle
    		* Empty inputs
    		* Count the number of finished pomodoros properly
    		* Recursive calls to `set_pomodoro()` to enter Pomodoros w/o
    		leaving the application.

--- Day-080/pomodoro-2.py
+++ Day-080/pomodoro-2.py
@@ -2,30 +2,49 @@
 
 import datetime
 
+pomodoro_count = 0
+
 
 def set_pomodoro():
     """
     Set a pomodoro
     """
-    pomodoro_count = 0
+    print("\n{:^50}".format("- Pomodoro Timer -"))
 
-    value = int(input("Enter your desired time in minutes: "))
+    # 1. Counter to note the pomodoros
+    # Global var set, so as to not be cleared while being updated.
+    global pomodoro_count
 
+    # 2. Safety checks
+    value = input("\nEnter your time in minutes: ")
+    if type(value) is not int:
+        try:
+            value = int(value)
+        except [ValueError]:
+            set_pomodoro()
+    elif value == "":
+        set_pomodoro()
+
+    # 3. Setting the current time
     time_now = datetime.datetime.now()
-    print("\nThe time is {}".format(time_now))
+    print("\n* Current time: {}".format(time_now))
 
+    # 4. Setting the timedelta, ie. the time in future
     set_time = datetime.timedelta(minutes=value)
 
+    # 5. Finding the difference between the current time and future time
     delta = time_now + set_time
-    print("The Pomodoro will finish at {}".format(delta))
+    print("* Finishing at: {}\n".format(delta))
 
+    # 6. Waiting till the current time reaches the future time
+    # We finish the set pomodoro if current time equals to future time.
     while True:
+
         if delta == datetime.datetime.now():
             print("Time up! {} minutes completed!".format(value))
             pomodoro_count += 1
-            break
-
-    print("Successfully completed {} pomodoro!".format(pomodoro_count))
+            print("\nPomodoros completed: {}".format(pomodoro_count))
+            set_pomodoro()
 
 
 if __name__ == "__main__":
