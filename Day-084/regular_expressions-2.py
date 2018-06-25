dbca0b95d55a023326c4d715380ddc4181af1074
Vimal A.R <arvimal@yahoo.in>
2018-06-25 22:54:28 +0530

    * Day 84 of #100DaysofCode
    	* Added Day-084/regular_expressions-2.py

---
+++ Day-084/regular_expressions-2.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+
+# Exercise from `Automate the boring stuff with Python`
+
+import pyperclip
+import re
+
+phoneRegex = re.compile(r'''(
+    (\d{3}|\(\d{3}\))?                # area code
+    (\s|-|\.)?                        # separator
+    (\d{3})                           # first 3 digits
+    (\s|-|\.)                         # separator
+    (\d{4})                           # last 4 digits
+    (\s*(ext|x|ext.)\s*(\d{2,5}))?    # extension
+    )''', re.VERBOSE)
