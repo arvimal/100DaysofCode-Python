8cb96cf332be8d46db9d1ea41158e788c78c0aa9
arvimal <arvimal@yahoo.in>
2018-10-13 12:11:56 +0530

    * Added hiCal.py and print.py

---
+++ Day-089/print.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+name = input("What is your name? ")
+quest = input("What is your quest? ")
+color = input("What is your favorite color? ")
+
+print("Your name is {}, quest is {}, and color is {}".format(name, quest, color))
