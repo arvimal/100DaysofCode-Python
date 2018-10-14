2c296b12cf74904a6dce23cd42359d183d4b2ad1
arvimal <arvimal@yahoo.in>
2018-10-14 08:13:28 +0530

    * Added mad_libs.py

---
+++ Day-090/mad_libs.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+"""
+This program generates passages that are generated in mad-lib format
+Author: Katherin
+"""
+
+# The template for the story
+
+STORY = "This morning %s woke up feeling %s. 'It is going to be a %s day!' Outside, a bunch of %ss were protesting to keep %s in stores. They began to %s to the rhythm of the %s, which made all the %ss very %s. Concerned, %s texted %s, who flew %s to %s and dropped %s in a puddle of frozen %s. %s woke up in the year %s, in a world where %ss ruled the world."
+
+
+print("Starting Mad Libs")
+name = input("Enter a name: ")
+
+adj1 = input("Enter the first adjective: ")
+adj2 = input("Enter the second adjective: ")
+adj3 = input("Enter the third adjective: ")
+
+verb1 = input("Enter a verb: ")
+
+noun1 = input("Enter the first noun: ")
+noun2 = input("Enter the second noun: ")
+
+animal = input("Enter an Animal name: ")
+food = input("Enter a food name: ")
+fruit = input("Enter a fruit name: ")
+superhero = input("Enter a superhero name: ")
+country = input("Enter a country name: ")
+dessert = input("Enter a dessert name: ")
+year = input("Enter a year: ")
+
+
+print(STORY % (name, adj1, adj2, animal, food, verb1, noun1, fruit, adj3, name, superhero, name, country, name, dessert, name, year, noun2))
+
+
+
+
+
+
+
+
