23bf83092f36fff7e06b2a45f6a8b2873c4f82f5
arvimal <arvimal@yahoo.in>
2018-10-18 13:49:10 +0530

    * Updated Day-092/last_names.py

--- Day-092/last_names.py
+++ Day-092/last_names.py
@@ -1,13 +1,18 @@
+#!/usr/bin/env python3
+
+"""
+1. Create a list of author names from the string.
+2. Create a second list with the last names of the authors.
+"""
+
 authors = "Audre Lorde, Gabriela Mistral, Jean Toomer, An Qi, Walt Whitman, Shel Silverstein, Carmen Boullosa, Kamala Suraiyya, Langston Hughes, Adrienne Rich, Nikki Giovanni"
 
-author_names = authors.split(',')
-
-print("\nAuthor names: ")
+# 1. Author names as a list
+author_names = authors.split(",")
 print(author_names)
 
+# 2. Last names of Authors
 author_last_names = []
 for name in author_names:
-  author_last_names.append(name.split()[-1])
-
-print("\nAuthor last names:")
+  author_last_names.append(name.split(" ")[-1])
 print(author_last_names)
