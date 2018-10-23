fa178c780b5fcfa591722e854f047205835dcbec
arvimal <arvimal@yahoo.in>
2018-10-23 11:58:25 +0530

    * Second assertion.

---
+++ Day-095/assertion-2.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+"""
+Write an assert statement that triggers an AssertionError
+if the variable spam is an integer less than 10.
+"""
+
+def check_int(spam=None):
+    """
+    IMPORTANT:
+    Note that we are asserting (forcing) a condition,
+    and the Assert kicks in when the condition is not satisfied.
+    Hence, to raise the assert for any numbers less than 10,
+    we have to enforce the number to be greater than 10.
+
+    """
+    assert int(spam) > 10, "Integer value less than 10"
+
+
+for num in range(8, 15):
+    print("\nTrying {}\n".format(num))
+    check_int(num)
+
+"""
+check_int(1)
+check_int(10)
+check_int(15)
+"""
