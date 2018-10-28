af5420972e9857a1ab38a72119cf87b01f97536f
arvimal <arvimal@yahoo.in>
2018-10-28 21:13:16 +0530

    Convert two lists to one dict, and parse the key/values.

--- Day-096/lists_to_dict.py
+++ Day-096/lists_to_dict.py
@@ -11,6 +11,6 @@ caffeine = [64, 40, 0, 120]
 
 output_dict = {key:value for key, value in zip(drinks, caffeine)}
 
-print("\nItems and prices:-\n")
+print("\nItems and caffeine scale:-\n")
 for key in output_dict:
     print("{:<10}: {:<10}".format(key, output_dict[key]))
