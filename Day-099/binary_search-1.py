26d60afd469156753a976a4a3ac1acfb688011c8
arvimal <arvimal@yahoo.in>
2018-11-08 23:57:47 +0530

    * Binary Search

--- Day-099/binary_search-1.py
+++ Day-099/binary_search-1.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
 """
-Binary Search
+Binary Search:-
+
+Binary Search is faster compared to Linear Search,
+since it divides the data set into half in each iteration.
+Thus the time compllex increases in the logarithmic rate.
 """
 
 def binary_search():
