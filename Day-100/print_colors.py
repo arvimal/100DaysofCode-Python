90e576cfe56f4fb735fd78cb6353f8f55780f360
arvimal <arvimal@yahoo.in>
2018-11-11 10:23:16 +0530

    * Regular expressions and PyBites

---
+++ Day-100/print_colors.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+VALID_COLORS = ['blue', 'yellow', 'red']
+
+
+def print_colors():
+    """Ask for color, lowercase it, check if 'quit' is entered, if so print
+       'bye' and break, next check if given color is in VALID_COLORS, if not,
+       continue, finally if that check passes, print the color"""
+    while True:
+        my_color = input("Enter a color:- ")
+        if my_color.lower() == "quit":
+            print("bye")
+            break
+        elif my_color.lower() in VALID_COLORS:
+            print("{}".format(my_color.lower()))
+        else:
+            print("Not a valid color")
+            pass
+
+
+print_colors()
