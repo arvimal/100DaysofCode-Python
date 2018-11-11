08fd567cefd891d7b15ead97be96a0b1c7f3fe77
arvimal <arvimal@yahoo.in>
2018-11-11 10:36:37 +0530

    * Pluralize words based on counts.

---
+++ Day-100/pluralize_words.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+"""
+Loop through a dict and pluralise the words
+"""
+
+games_won = dict(sara=0, bob=1, tim=5, julian=3, jim=1)
+
+
+def print_game_stats(games_won=games_won):
+    """Loop through games_won's dict k, v pairs (items) and
+       print how many games each person has won, pluralize
+       'game' based on number"""
+    for key in games_won:
+        if games_won[key] in [1]:
+            print("{} won {} game.".format(key, games_won[key]))
+        else:
+            print("{} won {} games.".format(key, games_won[key]))
+
+print_game_stats()
