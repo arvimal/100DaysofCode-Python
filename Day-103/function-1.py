095a98ae51289214692451e348a3edcc0a0d652a
Vimal A R <arvimal@yahoo.in>
2019-10-20 16:46:53 +0200

    Starting #100DaysofCode for 2019

---
+++ Day-103/function-1.py
@@ -0,0 +1,8 @@
+def sing_song():
+  print("You may say I'm a dreamer")
+  print("But I'm not the only one")
+  print("I hope some day you'll join us")
+  print("And the world will be as one")
+
+# call sing_song() below:
+sing_song()
