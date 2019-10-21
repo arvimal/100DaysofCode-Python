013991fd3daf32468563327366b61ac644ca2361
Vimal A R <arvimal@yahoo.in>
2019-10-21 08:43:01 +0200

    * if, elif, else.. part II

---
+++ Day-103/function-11.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+def grade_converter(gpa):
+  if gpa >= 4.0:
+    grade = "A"
+  elif gpa >= 3.0:
+    grade = "B"
+  elif gpa >= 2.0:
+    grade = "C"
+  elif gpa >= 1.0:
+    grade = "D"
+  elif gpa >= 0.0:
+    grade = "F"
+  return grade
+
+grade_converter(10)
