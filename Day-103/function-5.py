6360e582c869425d5b87d91cfb7dd30a77e2ac55
Vimal A R <arvimal@yahoo.in>
2019-10-21 06:52:27 +0200

    * Return multiples.

---
+++ Day-103/function-5.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+# Print the first three multiples of a given number `num`,
+# and finally return the third multiple.
+def first_three_multiples(num):
+  for i in range(1, 4):
+    print(num * i)
+  return num * i
+
+first_three_multiples(10)
+first_three_multiples(15)
