6339bafb45347bca9f5e4073cc75b6ed5da317a5
Vimal A R <arvimal@yahoo.in>
2019-10-21 07:53:14 +0200

    * Conditionals / loops

---
+++ Day-103/function-8.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+# Conditional loops
+
+def greater_than(x, y):
+  if x > y:
+    return x
+  elif y > x:
+    return y
+  elif x == y:
+    return "These numbers are the same"
+  
+def graduation_reqs(credits):
+  if credits >= 120:
+    return "You have enough credits to graduate!"
+  
+print(graduation_reqs(120))
