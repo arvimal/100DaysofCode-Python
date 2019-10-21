66dd870814ffe88229b61a74e1a891f4c7524031
Vimal A R <arvimal@yahoo.in>
2019-10-21 06:58:40 +0200

    * Human age in dog years :)

---
+++ Day-103/function-6.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+# Return your age in dog years :)
+# Assuming a human year is equivalent to seven dog years.
+
+def dog_years(name, age):
+  return ("{}, you are {} years old in dog years".format(name, age * 7))
+
+print(dog_years("Lola", 16))
+print(dog_years("Baby", 0))
