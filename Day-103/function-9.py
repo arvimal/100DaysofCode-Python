c97c4bcbfde971e83723469ad23f74be241b5cc9
Vimal A R <arvimal@yahoo.in>
2019-10-21 08:02:14 +0200

    * Conditionals

---
+++ Day-103/function-9.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+statement_one = (2 + 2 + 2 >= 6) and (-1 * -1 < 0)
+
+statement_two = (4 * 2 <= 8) and (7 - 1 == 6)
+
+def graduation_reqs(gpa, credits):
+  if credits >= 120 and gpa >= 2.0:
+    return "You meet the requirements to graduate!"
+  
+print(graduation_reqs(1.0, 125))
+print(graduation_reqs(1.4, 15))
+print(graduation_reqs(5.0, 155))
