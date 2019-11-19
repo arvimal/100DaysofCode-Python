a3f8f3e7526ac17ab7b43237d0930aff3eb78434
Vimal A R <arvimal@yahoo.in>
2019-11-19 07:02:12 +0100

    * Testing commits

---
+++ Day-105/max_number.py
@@ -0,0 +1,5 @@
+def not_sum_to_ten(num1, num2):
+    if num1 + num2 != 10:
+      return True
+    else:
+      return False
---
+++ Day-105/names.py
@@ -0,0 +1,6 @@
+# Write your same_name function here:
+def same_name(your_name, my_name):
+  if your_name == my_name:
+    return True
+  else:
+    return False
