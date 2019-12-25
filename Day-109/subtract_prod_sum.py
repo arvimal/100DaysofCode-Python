05d01162c1f8b67b9476c41c5abee112e9377493
Vimal A R <arvimal@yahoo.in>
2019-12-25 20:45:46 +0100

    LeetCode problem 1281 - Easy
    https://leetcode.com/problems/subtract-the-product-and-sum-of-digits-of-an-integer/

---
+++ Day-109/subtract_prod_sum.py
@@ -0,0 +1,16 @@
+class Solution:
+    def subtractProductAndSum(self, n: int) -> int:
+        num_list = []
+        for num in str(n):
+            num_list.append(int(num))
+        sum_num = sum(num_list)
+        prod_num = 1
+        for i in num_list:
+            prod_num = prod_num * i
+        print(prod_num - sum_num)
+        return prod_num - sum_num
+
+Test = Solution()
+Test.subtractProductAndSum(124)
+Test.subtractProductAndSum(234)
+Test.subtractProductAndSum(590)
