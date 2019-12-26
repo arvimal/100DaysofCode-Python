6ffbba62b894ef273a76b13a22f6be3586812cd5
Vimal A R <arvimal@yahoo.in>
2019-12-26 21:17:42 +0100

    HackerRank problem - Leap year

---
+++ Day-109/leap_year.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+# HackerRank Problem
+# https://www.hackerrank.com/challenges/write-a-function/problem
+
+
+def is_leap(year):
+    leap = False
+
+
+    return leap
+
+
+year = int(input())
