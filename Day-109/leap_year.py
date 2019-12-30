bd499d3f754050f0f2b32641d3915151f5a49ac3
Vimal A R <arvimal@yahoo.in>
2019-12-30 22:34:20 +0100

    HackerRank Python challenge - Leap year

--- Day-109/leap_year.py
+++ Day-109/leap_year.py
@@ -3,12 +3,29 @@
 # HackerRank Problem
 # https://www.hackerrank.com/challenges/write-a-function/problem
 
+# Source: Wikipedia - https://en.wikipedia.org/wiki/Leap_year
+# The following pseudocode determines whether a year is a leap year or a common year
+# in the Gregorian calendar (and in the proleptic Gregorian calendar before 1582).
+# The year variable being tested is the integer representing the number of the year
+# in the Gregorian calendar.
+
+# if (year is not divisible by 4) then(it is a common year)
+# else if (year is not divisible by 100) then(it is a leap year)
+# else if (year is not divisible by 400) then(it is a common year)
+# else (it is a leap year)
+
 
 def is_leap(year):
     leap = False
-
-
+    if year % 4 != 0:
+        pass
+    elif year % 100 != 0:
+        leap = True
+    elif year % 400 != 0:
+        leap = False
+    else:
+        leap = True
+    print(leap)
     return leap
 
-
-year = int(input())
+is_leap(2000)
