51b2d607ed3c642e6975dfc0067de14ab89b4c17
Vimal A R <arvimal@yahoo.in>
2019-12-31 22:40:00 +0100

    Big-O - Interviecake

---
+++ Day-111/big_O-1.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+# Big O example 1
+
+# O(1) or Constant time
+
+items = ["bread", "milk", "scuba mix", "fruittella", "rice", "cucumber", "bonne mamman fig jam"]
+
+def print_first_item(items):
+    print(items[0])
+    print(items[2])
+    print(items[6])
+
+print("m,n guyh ckuof8uyt clg8ruyt8r594dyt8ilrcf4ylhuynir")
+
+
+print_first_item(items)
+
+
