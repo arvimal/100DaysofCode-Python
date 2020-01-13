01c5a53a34622c83fdc4583266c2dcb7a763e81f
Vimal A R <arvimal@yahoo.in>
2020-01-13 20:07:06 +0100

    HackerRank problem

---
+++ Day-112/class_instance.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+# HackerRank 30-days of Code
+
+
+class Person:
+    def __init__(self, initialAge):
+        pass
+
+
+    def amIOld(self):
+        pass
+
+
+    def yearPasses(self):
+        pass
+
+
+
+t = int(input())
