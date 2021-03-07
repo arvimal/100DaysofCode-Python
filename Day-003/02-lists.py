506097ebb14942af1193f235813f00c4df70a629
Vimal A R <arvimal@yahoo.in>
2021-03-07 20:19:21 +0100

    Updates to lists

--- Day-003/02-lists.py
+++ Day-003/02-lists.py
@@ -18,4 +18,20 @@ list_5 = [[1, 2, 3], [5, 6, 1]]
 list_6 = list_5.__add__(list_4)  # Or list_5 + list_4
 print(list_6)
 
-# print(dir())
+print(dir())
+
+# Create a copy of a list
+#
+# 1. Use `=`
+# This is an exact copy, which means any change in one goes in the other
+# The new list name is just a reference to the same memory address
+list_copy_1 = list_4
+print(id(list_copy_1))
+print(id(list_4))
+list_copy_1 is list_4
+#
+# 2. Add the elements, rather than assigning the list directly
+list_copy_2 = list_4[:]
+print(id(list_copy_1))
+print(id(list_4))
+list_copy_1 is list_4
